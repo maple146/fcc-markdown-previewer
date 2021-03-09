ea702ab091477d7469a26e63ec3c2e7825ac8321
Luciano Alcaraz <LGA-dev@hotmail.com>
2021-03-09 18:23:13 -0300

    Add basic preview functionality

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react';
 
 function App() {
+  const [textAreaValue, setTextAreaValue] = useState("")
+
+  const handleChange = (event) => {
+    setTextAreaValue(event.target.value)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="editor">
+        <h1>Editor</h1>
+        <textarea name="editor" id="editor" cols="30" rows="10" defaultValue="Blabla" onChange={handleChange}></textarea>
+      </div>
+      <div className="preview">
+        <h1>Preview</h1>
+        <h2 id="preview">{textAreaValue}</h2>
+      </div>
     </div>
   );
 }
